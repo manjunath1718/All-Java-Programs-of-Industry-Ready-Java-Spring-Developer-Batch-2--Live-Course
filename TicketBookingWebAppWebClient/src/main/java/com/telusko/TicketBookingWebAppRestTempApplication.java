cf9664c652a189ab11c4826e6406210c5f690df7
manjunath <betagerimanju7@gmail.com>
2024-08-25 14:29:18 +0530

    RestTemplate and WebClient program

---
+++ TicketBookingWebAppWebClient/src/main/java/com/telusko/TicketBookingWebAppRestTempApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingWebAppRestTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingWebAppRestTempApplication.class, args);
+	}
+
+}
---
+++ TicketBookingWebAppWebClient/src/main/java/com/telusko/controller/TicketBookingController.java
@@ -0,0 +1,50 @@
+package com.telusko.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.telusko.model.Passenger;
+import com.telusko.model.Ticket;
+import com.telusko.service.ITicketBookingService;
+
+@Controller
+public class TicketBookingController {
+
+	@Autowired
+	private ITicketBookingService service;
+	
+	@PostMapping("/book-ticket")
+	public String bookTicket(@ModelAttribute Passenger passenger, Model model){
+		
+		Ticket ticket = service.registerPassenger(passenger);
+		model.addAttribute("ticketNumber", ticket.getTicketNumber());
+		return "index";//lvn
+	}
+	
+	@GetMapping("/form")
+	public String getForm(Model model){
+		
+		model.addAttribute("passenger", new Passenger());
+		return "index";
+	}
+	
+	@GetMapping("/ticket")
+	public String getTicketForm(Model model){
+		
+		model.addAttribute("ticket", new Ticket());
+		return "ticket-form";
+	}
+	
+	@GetMapping("/get-ticket")
+	public String getTicketInfo(@RequestParam("ticketNumber")Integer ticketNumber,Model model){
+		
+		Ticket ticket=service.getFullTicketInfo(ticketNumber);
+		model.addAttribute("ticket", ticket);		
+		return "ticket-info";
+	}
+}
